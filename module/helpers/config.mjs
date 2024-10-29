3da48f46aedeb4c68651de38daff1b888bf0542a
Andrew Beard <bearda@gmail.com>
2024-10-29 15:33:09 -0400

    Initial checkin

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const NEON_LORDS = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+NEON_LORDS.abilities = {
+  str: 'NEON_LORDS.Ability.Str.long',
+  dex: 'NEON_LORDS.Ability.Dex.long',
+  con: 'NEON_LORDS.Ability.Con.long',
+  int: 'NEON_LORDS.Ability.Int.long',
+  wis: 'NEON_LORDS.Ability.Wis.long',
+  cha: 'NEON_LORDS.Ability.Cha.long',
+};
+
+NEON_LORDS.abilityAbbreviations = {
+  str: 'NEON_LORDS.Ability.Str.abbr',
+  dex: 'NEON_LORDS.Ability.Dex.abbr',
+  con: 'NEON_LORDS.Ability.Con.abbr',
+  int: 'NEON_LORDS.Ability.Int.abbr',
+  wis: 'NEON_LORDS.Ability.Wis.abbr',
+  cha: 'NEON_LORDS.Ability.Cha.abbr',
+};
