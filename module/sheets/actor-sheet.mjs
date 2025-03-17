11adaf0bc9760ea6ec7b537be44f51b237f9de2b
Andrew Beard <bearda@gmail.com>
2025-03-17 10:32:52 -0400

    Fixed deprecation warning

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -214,7 +214,7 @@ export class NeonLordsActorSheet extends ActorSheet {
     // Get the type of item to create.
     const type = header.dataset.type;
     // Grab any data associated with this control.
-    const data = duplicate(header.dataset);
+    const data = foundry.utils.duplicate(header.dataset);
     // Initialize a default name.
     const name = `New ${type.capitalize()}`;
     // Prepare the item object.
