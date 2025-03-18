f4d7d5b2abc2c6295910af42d8de862eed64e7de
Andrew Beard <bearda@gmail.com>
2025-03-18 19:37:09 -0400

    Added saves to the NPC sheet

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -18,7 +18,7 @@ export class NeonLordsActorSheet extends ActorSheet {
         {
           navSelector: '.sheet-tabs',
           contentSelector: '.sheet-body',
-          initial: 'features',
+          initial: 'attributes',
         },
       ],
     });
